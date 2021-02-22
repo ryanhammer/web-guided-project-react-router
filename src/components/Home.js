be34445e8396498d11cfbccddda00c95717e487c
Andreas Bolos <andreasbolos@Andreass-MacBook-Pro.local>
2021-02-22 11:12:12 -0700

    wip

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import React from 'react'
 // We'll need React Router's own version of the History API
+import { useHistory } from 'react-router-dom'
 
 export default function Home() {
   // 👉 STEP 5 - Build a click handler that will imperatively
   // navigate us to <website base URL>/items-list
+  const history = useHistory()
 
   const routeToShop = () => {
-
+    console.log(history);
+    history.push('/items-list')
   }
 
   return (
