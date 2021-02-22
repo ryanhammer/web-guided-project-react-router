32eccd0aabea73cc7cf337aa695b3cb979268685
Andreas Bolos <andreasbolos@Andreass-MacBook-Pro.local>
2021-02-22 10:46:50 -0700

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 // 👉 STEP 2 - React Router imports (Route, Link and Switch)
+import { Route, Link, Switch } from 'react-router-dom'
 
 // Components used for the different routes
 import Home from './Home'
