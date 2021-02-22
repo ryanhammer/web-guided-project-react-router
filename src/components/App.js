0d174da552dce81d4c1743b7a366256db94d56b9
Andreas Bolos <andreasbolos@Andreass-MacBook-Pro.local>
2021-02-22 10:52:16 -0700

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -34,8 +34,11 @@ export default function App(props) {
       </nav>
 
       {/* 👉 STEP 4 - Build a Switch with a Route for each of the components imported at the top */}
-      <Route>
+      <Route path="/">
         <Home />
+      </Route>
+      <Route path="/items-list">
+        <ItemsList />
       </Route>
     </div>
   )
